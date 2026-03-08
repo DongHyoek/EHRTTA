3f05a47ac2a05e82b63f9858f4064f9123e7c689
DongHyoek <inuyasha0204@gmail.com>
2026-03-08 21:35:27 +0900

    not use summarize tokens in LLM inputs

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,15 +6,28 @@ ulimit -s unlimited
 python main.py \
     --use_time  --use_norm_ema --align_return_weights --use_dora --cuda \
     --scheduler --early_stop \
-    --ckpt_dir ./results/checkpoint/wo_learnable_summarize_tokens \
-    --metrics_dir ./results/metrics/wo_learnable_summarize_tokens \
+    --ckpt_dir ./results/checkpoint/wo_learnable_summarize_tokens_last_pool \
+    --metrics_dir ./results/metrics/wo_learnable_summarize_tokens_last_pool \
     --te_n_vars 45 --te_n_fields 11 --te_n_texts 414 \
     --n_epochs 50 --lr 0.0001 --patience 5 \
     --batch_size 8 --h_pool last \
     --model_id meta-llama/Llama-3.2-1B \
     --use_ts_trunc --max_length 60 --te_cls_init raw_tok
 
-## Dora 사용 X (LoRA로만 활용)
+# mean구조
+python main.py \
+    --use_time  --use_norm_ema --align_return_weights --use_dora --cuda \
+    --scheduler --early_stop \
+    --ckpt_dir ./results/checkpoint/wo_learnable_summarize_tokens_mean_pool \
+    --metrics_dir ./results/metrics/wo_learnable_summarize_tokens_mean_pool \
+    --te_n_vars 45 --te_n_fields 11 --te_n_texts 414 \
+    --n_epochs 50 --lr 0.0001 --patience 5 \
+    --batch_size 8 --h_pool mean \
+    --model_id meta-llama/Llama-3.2-1B \
+    --use_ts_trunc --max_length 60 --te_cls_init raw_tok
+
+
+# # LoRA 사용 
 # python -X faulthandler main.py \
 #     --use_time  --use_norm_ema --align_return_weights --cuda \
 #     --scheduler --early_stop \
